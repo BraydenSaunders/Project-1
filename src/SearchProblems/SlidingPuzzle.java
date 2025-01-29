34ced438b2ed2d7e0806cdb134bd85cc59736709
Brayden Saunders <153005175+LightningUser24@users.noreply.github.com>
2025-01-29 17:42:33 -0500

    Prints a 2 dimensional array, though it still has the brackets

--- src/SearchProblems/SlidingPuzzle.java
+++ src/SearchProblems/SlidingPuzzle.java
@@ -22,24 +22,39 @@ public class SlidingPuzzle implements Problem<List<Integer>, List<Integer>> {
     public List<Integer> goalState(){
         return GOAL_STATE;
     }
-
-    //    public void printStateMatrix(List<Integer> matrix){
-//        for(int i = 0; i < INITIAL_STATE1.size(); i++){
-//            for(int j = 0; j < INITIAL_STATE1)
-//        }
-//    }
-    public void printState(List<Integer> list){
-        System.out.println(list);
+    public ArrayList<ArrayList<Integer>> geEmptyArrays(){
+        ArrayList<ArrayList<Integer>> listOfEmptyArrays = new ArrayList<>();
+        for (int i = 0; i < SIZE; i++){
+            listOfEmptyArrays.add(new ArrayList<>());
+        }
+        return listOfEmptyArrays;
     }
 
+
+    public void printState(List<Integer> list){
+        ArrayList<ArrayList<Integer>> matrix = geEmptyArrays();
+        int listIndex = 0;  // index to track elements in the list
+        for (int i = 0; i < matrix.size() && listIndex < list.size(); i++) {
+            ArrayList<Integer> currentRow = matrix.get(i);
+            // Add elements to the current row until it has 3 elements
+            for (int j = 0; j < SIZE && listIndex < list.size(); j++) {
+                currentRow.add(list.get(listIndex));
+                listIndex++;
+            }
+        }
+        for (int i = 0; i < matrix.size(); i++){
+            System.out.println(matrix.get(i));
+        }
+    }
+
+
     public List<Tuple<List<Integer>, List<Integer>>> execution(List<Integer> currentState){
-        List<Tuple<List<Integer>, List<Integer>>> result = new ArrayList<>();
-        if (!INITIAL_STATE.equals(GOAL_STATE)){
-            result.add(new Tuple<>(INITIAL_STATE, currentState));
-        }
-        else {
-            return null;
-        }
-        return result;
+
+    }
+
+    public static void main(String[] args) {
+        List<Integer> test = new ArrayList<>(List.of(7,2,4,5,0,6,8,3,1));
+        SlidingPuzzle slidingPuzzle = new SlidingPuzzle(3);
+        slidingPuzzle.printState(test);
     }
 }
