da893d9e1691f88cb8b825c84992b46d4bc1727f
Brayden Saunders <153005175+LightningUser24@users.noreply.github.com>
2025-01-29 17:42:33 -0500

    Prints a 2 dimensional array, though it still has the brackets

--- src/SearchSolutions/SlidingPuzzle_AStar_Distance.java
+++
@@ -1,4 +0,0 @@
-package SearchSolutions;
-
-public class Travel_A_Star {
-}
--- src/SearchSolutions/SlidingPuzzle_BFS.java
+++ src/SearchSolutions/SlidingPuzzle_BFS.java
@@ -1,4 +1,10 @@
 package SearchSolutions;
 
+import CoreSearch.FIFOQueue;
+import SearchProblems.SlidingPuzzle;
+
 public class SlidingPuzzle_BFS {
+    public SlidingPuzzle_BFS(int mapSize){
+
+    }
 }
