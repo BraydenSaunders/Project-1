30967dc5c602290f9ca3ab2bf49b9fc690c9d2de
Brayden Saunders <153005175+BraydenSaunders@users.noreply.github.com>
2025-01-29 09:07:10 -0500

    Add files via upload

---
+++ Project1-326/src/CoreSearch/Problem.java
@@ -0,0 +1,22 @@
+package CoreSearch;
+
+import java.util.List;
+
+/**
+ * This interface represents a generic search problem formulation.
+ *
+ *  Type parameters:
+ *      S: the data type of states
+ *      A: the data type of actions
+ */
+public interface Problem <S, A>{
+    S initialState();
+    S goalState();
+
+    //executes all the applicable actions at the state and
+    //returns a list of tuples of [new state, action taken, cost]
+    List<Tuple<S,A>> execution(S state);
+
+    //Prints out the state in a neat format
+    void printState(S state);
+}
---
+++ Project1-326/src/SearchProblems/SlidingPuzzle.java
@@ -0,0 +1,16 @@
+package SearchProblems;
+
+import CoreSearch.Problem;
+
+
+public class SlidingPuzzle implements Problem<String, String> {
+    private final String INITIAL_STATE = "{7,2,4,5,0,6,8,3,1}";
+
+    private final String GOAL_STATE = "{0,1,2,3,4,5,6,7,8}";
+
+    public SlidingPuzzle(){
+
+    }
+
+    public
+}
