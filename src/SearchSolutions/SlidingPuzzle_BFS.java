07355d6af845bde0f7c4d2b5aab67bc0f338717f
Brayden Saunders <153005175+LightningUser24@users.noreply.github.com>
2025-01-30 14:22:24 -0500

    Completed BFS and DFS functions

--- src/SearchSolutions/SlidingPuzzle_BFS.java
+++ src/SearchSolutions/SlidingPuzzle_BFS.java
@@ -1,10 +1,18 @@
 package SearchSolutions;
 
+import CoreSearch.BaseSearch;
 import CoreSearch.FIFOQueue;
 import SearchProblems.SlidingPuzzle;
 
-public class SlidingPuzzle_BFS {
-    public SlidingPuzzle_BFS(int mapSize){
+import java.util.List;
 
+public class SlidingPuzzle_BFS extends BaseSearch<List<Integer>, List<Integer>> {
+    public SlidingPuzzle_BFS(int mapSize){
+        super(new SlidingPuzzle(mapSize), new FIFOQueue<>());
+    }
+
+    public static void main(String[] args) {
+        SlidingPuzzle_BFS bfs = new SlidingPuzzle_BFS(3);
+        bfs.search();
     }
 }
--- src/SearchSolutions/SlidingPuzzle_DFS.java
+++ src/SearchSolutions/SlidingPuzzle_DFS.java
@@ -1,4 +1,18 @@
 package SearchSolutions;
 
-public class SlidingPuzzle_DFS {
+import CoreSearch.BaseSearch;
+import CoreSearch.FILOQueue;
+import SearchProblems.SlidingPuzzle;
+
+import java.util.List;
+
+public class SlidingPuzzle_DFS extends BaseSearch<List<Integer>,List<Integer>> {
+    public SlidingPuzzle_DFS(int mapSize){
+        super(new SlidingPuzzle(mapSize), new FILOQueue<>());
+    }
+
+    public static void main(String[] args) {
+        SlidingPuzzle_DFS dfs = new SlidingPuzzle_DFS(3);
+        dfs.search();
+    }
 }
