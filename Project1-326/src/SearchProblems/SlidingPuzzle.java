1ffb21c7ab2848c8d036e93fb32712aa3d7d9526
Saaaaaaaaaaaaaaaaaaaaaaa <11saaweathers5@gmail.com>
2025-01-31 00:47:27 -0500

    Initial Commit. Says there is error in Problem Class, can't figure it out.

--- Project1-326/src/SearchProblems/SlidingPuzzle.java
+++ Project1-326/src/SearchProblems/SlidingPuzzle.java
@@ -1,16 +1,65 @@
 package SearchProblems;
 
 import CoreSearch.Problem;
+import CoreSearch.Tuple;
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class SlidingPuzzle implements Problem<String, String> {
-    private final String INITIAL_STATE = "{7,2,4,5,0,6,8,3,1}";
+public class SlidingPuzzle implements Problem<List<Integer>, String> {
+    private final int SIZE = 3;
 
-    private final String GOAL_STATE = "{0,1,2,3,4,5,6,7,8}";
+    private final List<Integer> INITIAL_STATE = new ArrayList<>(
+            List.of(7,2,4, 5,0,6, 8,3,1));
 
-    public SlidingPuzzle(){
+    private final List<Integer> GOAL_STATE = new ArrayList<>(
+            List.of(0,1,2, 3,4,5, 6,7,8));
 
+    public List<Integer> initialState;
+
+    public SlidingPuzzle() {
+        this.initialState = new ArrayList<>(INITIAL_STATE);
     }
 
-    public
+
+    @Override
+    public void printState(List<Integer> state) {
+        printMatrix(state);
+    }
+    public void printMatrix(List<Integer> state) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(state.get(i * SIZE + j) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public List<Tuple<Integer, Integer>> execution(Integer index) {
+        List<Tuple<Integer, Integer>> moves = new ArrayList<>();
+        int row = index / SIZE, col = index % SIZE;
+
+        if (row > 0) moves.add(new Tuple<>(index, index - SIZE)); // Move up
+        if (row < SIZE - 1) moves.add(new Tuple<>(index, index + SIZE)); // Move down
+        if (col > 0) moves.add(new Tuple<>(index, index - 1)); // Move left
+        if (col < SIZE - 1) moves.add(new Tuple<>(index, index + 1)); // Move right
+
+        return moves;
+    }
+
+
+    public static void main(String[] args) {
+        SlidingPuzzle puzzle = new SlidingPuzzle();
+
+        System.out.println("Initial State:");
+        puzzle.printMatrix(puzzle.initialState);
+
+        int emptyIndex = puzzle.initialState.indexOf(0);
+        System.out.println("Possible Moves:");
+
+        for (Tuple<Integer, Integer> move : puzzle.execution(emptyIndex)) {
+            System.out.println("Move 0 from " + move.getState() + " to " + move.getAction());
+        }
+    }
 }
