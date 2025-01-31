362ae0d1431fff33eac0e447e5012f5cec64808f
Brayden Saunders <153005175+LightningUser24@users.noreply.github.com>
2025-01-31 13:20:02 -0500

    UCS now works

--- src/SearchSolutions/SlidingPuzzle_UCS.java
+++ src/SearchSolutions/SlidingPuzzle_UCS.java
@@ -1,4 +1,33 @@
 package SearchSolutions;
 
-public class SlidingPuzzle_UCS {
+import CoreSearch.BaseSearch;
+import CoreSearch.Node;
+import CoreSearch.SortedQueue;
+import SearchProblems.SlidingPuzzle;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class SlidingPuzzle_UCS extends BaseSearch<List<Integer>, Integer> {
+    public SlidingPuzzle_UCS(int mapSize){
+        super(new SlidingPuzzle(mapSize), new SortedQueue<>(new ComparePathCost()));
+    }
+
+    public static void main(String[] args) {
+        SlidingPuzzle_UCS ucs = new SlidingPuzzle_UCS(3);
+        ucs.search();
+    }
+    public static class ComparePathCost implements Comparator<Node<List<Integer>, Integer>>{
+        public int compare(Node<List<Integer>, Integer> o1, Node<List<Integer>, Integer> o2){
+            if (o1.getPathCost() < o2.getPathCost()){
+                return -1;
+            }
+            else if (o1.getPathCost() == o2.getPathCost()){
+                return 0;
+            }
+            else {
+                return 1;
+            }
+        }
+    }
 }
