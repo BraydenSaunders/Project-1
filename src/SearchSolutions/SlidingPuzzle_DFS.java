14e0be348be0759ec3764c98ac15b0dfe45ab51c
Brayden Saunders <153005175+LightningUser24@users.noreply.github.com>
2025-01-31 12:59:20 -0500

    BFS now works

--- src/SearchSolutions/SlidingPuzzle_DFS.java
+++ src/SearchSolutions/SlidingPuzzle_DFS.java
@@ -6,13 +6,13 @@ import SearchProblems.SlidingPuzzle;
 
 import java.util.List;
 
-//public class SlidingPuzzle_DFS extends BaseSearch<List<Integer>,List<Integer>> {
-//    public SlidingPuzzle_DFS(int mapSize){
-//        super(new SlidingPuzzle(mapSize), new FILOQueue<>());
-//    }
-//
-//    public static void main(String[] args) {
-//        SlidingPuzzle_DFS dfs = new SlidingPuzzle_DFS(3);
-//        dfs.search();
-//    }
-//}
+public class SlidingPuzzle_DFS extends BaseSearch<List<Integer>,Integer> {
+    public SlidingPuzzle_DFS(int mapSize){
+        super(new SlidingPuzzle(mapSize), new FILOQueue<>());
+    }
+
+    public static void main(String[] args) {
+        SlidingPuzzle_DFS dfs = new SlidingPuzzle_DFS(3);
+        dfs.search();
+    }
+}
