5cd38e1a9a181b0fe8892d6b3dac0e8c23d5051c
Saaaaaaaaaaaaaaaaaaaaaaa <11saaweathers5@gmail.com>
2025-02-02 01:45:03 -0500

    Finished the Distance Classes. Two more SearchSolution classes left to finish

--- src/SearchSolutions/SlidingPuzzle_AStar_Distances.java
+++ src/SearchSolutions/SlidingPuzzle_AStar_Distances.java
@@ -1,4 +1,49 @@
 package SearchSolutions;
 
-public class SlidingPuzzle_AStar_Distances {
+import CoreSearch.BaseSearch;
+import CoreSearch.Node;
+import CoreSearch.SortedQueue;
+import SearchProblems.SlidingPuzzle;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class SlidingPuzzle_AStar_Distances extends BaseSearch<List<Integer>, Integer> {
+    public SlidingPuzzle_AStar_Distances() {
+        super(new SlidingPuzzle(), new SortedQueue<>(new ComparePathCost()));
+    }
+
+
+    protected int heuristic(Node<List<Integer>, Integer> node) {
+        List<Integer> state = node.getState();
+        int distance = 0;
+
+        for (int i = 0; i < state.size(); i++) {
+            int value = state.get(i);
+            if (value != 0) { // Ignore the blank tile
+                int targetRow = value / 3;
+                int targetCol = value % 3;
+                int currentRow = i / 3;
+                int currentCol = i % 3;
+                distance += Math.abs(targetRow - currentRow) + Math.abs(targetCol - currentCol);
+            }
+        }
+        return distance;
+    }
+
+    public static void main(String[] args) {
+        SlidingPuzzle_AStar_Distances astar = new SlidingPuzzle_AStar_Distances();
+        astar.search();
+    }
+
+    // Comparator for A* which considers both path cost and heuristic
+    public static class ComparePathCost implements Comparator<Node<List<Integer>, Integer>> {
+        public int compare(Node<List<Integer>, Integer> o1, Node<List<Integer>, Integer> o2) {
+            // A* comparison: f(n) = g(n) + h(n)
+            int f1 = o1.getPathCost() + new SlidingPuzzle_AStar_Distances().heuristic(o1);
+            int f2 = o2.getPathCost() + new SlidingPuzzle_AStar_Distances().heuristic(o2);
+
+            return Integer.compare(f1, f2);
+        }
+    }
 }
--- src/SearchSolutions/SlidingPuzzle_GBFS_Distance.java
+++ src/SearchSolutions/SlidingPuzzle_GBFS_Distance.java
@@ -1,5 +1,20 @@
 package SearchSolutions;
 
-public class SlidingPuzzle_GBFS_Distance {
+import CoreSearch.BaseSearch;
+import CoreSearch.SortedQueue;
+import SearchProblems.SlidingPuzzle;
+import java.util.*;
 
+public class SlidingPuzzle_GBFS_Distance extends BaseSearch<List<Integer>, Integer> {
+
+    public SlidingPuzzle_GBFS_Distance(SlidingPuzzle problem) {
+        super(new SlidingPuzzle(), new SortedQueue<>(new SlidingPuzzle_UCS.ComparePathCost()));
+    }
+
+    public static void main(String[] args) {
+        SlidingPuzzle problem = new SlidingPuzzle();
+        SlidingPuzzle_GBFS_Distance search = new SlidingPuzzle_GBFS_Distance(problem);
+        boolean found = search.search();
+        System.out.println("Goal Found: " + found);
+    }
 }
